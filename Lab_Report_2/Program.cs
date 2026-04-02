73ac8dc5a2391038930a44f27564b3c243facd11
Tashi Sherpa <sherpajack3@gmail.com>
2026-04-02 20:05:38 +0545

    Lab report 3

--- Lab_Report_2/Program.cs
+++ Lab_Report_2/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Student
+{
+    public string name;
+    public int marks;
+}
+
+class Program
+{
+    // Method to modify value type
+    static void ModifyValue(int marks)
+    {
+        marks = marks + 10;
+        Console.WriteLine("Inside Value Method: " + marks);
+    }
+
+    // Method to modify reference type
+    static void ModifyReference(Student s)
+    {
+        s.marks = s.marks + 10;
+        Console.WriteLine("Inside Reference Method: " + s.marks);
+    }
+
+    static void Main(string[] args)
+    {
+        // Value type
+        int marks = 50;
+
+        // Reference type
+        Student s = new Student();
+        s.name = "Tashi";
+        s.marks = 50;
+
+        Console.WriteLine("Before Value Method: " + marks);
+        ModifyValue(marks);
+        Console.WriteLine("After Value Method: " + marks);
+
+        Console.WriteLine();
+
+        Console.WriteLine("Before Reference Method: " + s.marks);
+        ModifyReference(s);
+        Console.WriteLine("After Reference Method: " + s.marks);
+
+        Console.ReadLine();
+    }
+}
