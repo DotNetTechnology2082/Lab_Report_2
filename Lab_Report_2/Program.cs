17b4d01a686ff267dab305cc7e8b75d547247906
SANDESH <sandeshbca23@oic.edu.np>
2026-04-03 08:21:41 +0545

    lab reprt 2

--- Lab_Report_2/Program.cs
+++ Lab_Report_2/Program.cs
@@ -1,1 +1,47 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Student
+{
+    public string name;
+    public int marks;
+}
+
+class Program
+{
+    // Method to modify value type
+    static void ModifyValue(int num)
+    {
+        num = num + 50;
+        Console.WriteLine("Inside ModifyValue: " + num);
+    }
+
+    // Method to modify reference type
+    static void ModifyReference(Student s)
+    {
+        s.name = "Changed Name";
+        s.marks = s.marks + 50;
+        Console.WriteLine("Inside ModifyReference: " + s.name + " - " + s.marks);
+    }
+
+    static void Main(string[] args)
+    {
+        // VALUE TYPE
+        int marks = 50;
+        Console.WriteLine("Before ModifyValue: " + marks);
+        ModifyValue(marks);
+        Console.WriteLine("After ModifyValue: " + marks);
+
+        Console.WriteLine();
+
+        // REFERENCE TYPE
+        Student student = new Student();
+        student.name = "Sandesh Bhusal";
+        student.marks = 50;
+
+        Console.WriteLine("Before ModifyReference: " + student.name + " - " + student.marks);
+        ModifyReference(student);
+        Console.WriteLine("After ModifyReference: " + student.name + " - " + student.marks);
+
+        Console.ReadLine();
+    }
+}
